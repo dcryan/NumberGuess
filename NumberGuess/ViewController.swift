b42edb09fc73c271d09bb23e55e502e4e4f80867
Daniel Ryan <daniel.c.ryan@icloud.com>
2014-12-07 19:17:59 -0700

    Added functionality for the submit button. Add a game board initialization. Added a reset button.

--- NumberGuess/ViewController.swift
+++ NumberGuess/ViewController.swift
@@ -12,9 +12,16 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var guessText: UITextField!
     @IBOutlet weak var feedBackText: UILabel!
+    @IBOutlet weak var retryButton: UIButton!
+    
+    var randomNumber:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
+        newGame()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,7 +30,30 @@ class ViewController: UIViewController {
     }
 
     @IBAction func submitGuess(sender: AnyObject) {
+        var guessNumber = guessText.text.toInt()
+        
+        if(guessNumber == randomNumber)
+        {
+            feedBackText.text = "Got it!"
+            retryButton.hidden = false
+        }
+        else
+        {
+            feedBackText.text = "Try again..."
+        }
+
     }
 
+    @IBAction func restart(sender: AnyObject) {
+        newGame()
+    }
+    
+    func newGame()
+    {
+        randomNumber = Int(arc4random() % 6)
+        println(randomNumber)
+        feedBackText.text = ""
+        retryButton.hidden = true
+    }
 }
 
