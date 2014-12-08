422d13360f1cfa74348ad2b9e4977c8245a368ac
Daniel Ryan <daniel.c.ryan@icloud.com>
2014-12-07 17:59:19 -0700

    added the skeleton of the project.

--- NumberGuess/ViewController.swift
+++ NumberGuess/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var guessText: UITextField!
+    @IBOutlet weak var feedBackText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func submitGuess(sender: AnyObject) {
+    }
 
 }
 
